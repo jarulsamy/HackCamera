7b1010ce5273f28a54b169ec24742cb27dc20aab
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2015-09-23 16:53:14 -0600

    Images are't saved and new pictures come and go

--- cam.py
+++ cam.py
@@ -2,8 +2,9 @@ import cv2
 #from SimpleCV import Image, Camera
 
 camera = cv2.VideoCapture(0)
-image = cv2.imread("filename.jpg", cv2.CV_LOAD_IMAGE_COLOR)
+im = cv2.imread("filename.jpg", cv2.CV_LOAD_IMAGE_COLOR)
 retval, im = camera.read()
+#cv2.imwrite("filename.jpg",im)
 cv2.namedWindow('Display Window')
-cv2.imshow('Display Window', image)
+cv2.imshow('Display Window', im)
 cv2.waitKey(0)
